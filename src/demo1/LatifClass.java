2c8d67f02659cc97fd3bc913a4376d527f7767d6
khsed <khsed@10.0.0.89>
2023-07-27 22:21:05 -0400

    This is Latif Project

---
+++ src/demo1/LatifClass.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class LatifClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The way on how to create a local repository is , first create a project,"
+				+ " then a package and then a class and then add main method and then save and close");
+		
+		System.out.println("Live is short");
+	}
+
+}
